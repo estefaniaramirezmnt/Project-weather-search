b1232d2780195ef5667dd7d340fe6ef7aea2e5b4
Estefanía <estefaniaramirezmnt@gmail.com>
2023-10-14 21:30:56 +0200

    Formatted hour and date

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-function displayTemperature(response) {
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wensday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ]
+    let day = days[date.getDay()];
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+  function displayTemperature(response) {
     let temperatureElement = document.getElementById("temperature");
     let cityElement = document.getElementById("city");
     let descriptionElement = document.getElementById("description");
     let humidityElement = document.getElementById("humidity");
     let windElement = document.getElementById("wind");
+    let dateElement = document.getElementById("date");
   
     temperatureElement.textContent = Math.round(
       response.data.temperature.current
     );
     cityElement.textContent = response.data.city;
-    descriptionElement.textContent = response.data.condition.description.charAt(0).toUpperCase() + response.data.condition.description.slice(1);
+    descriptionElement.textContent = response.data.condition.description;
     humidityElement.textContent = response.data.temperature.humidity;
     windElement.textContent = Math.round(response.data.wind.speed);
+    dateElement.textContent = formatDate(response.data.time * 1000);
   }
   
   let apiKey = "4efbbf43t600f8b07428238a0a4o0852";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New%20York&key=${apiKey}&units=metric`;
+  let city = "New York";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   
   axios.get(apiUrl).then(displayTemperature);
   
-
