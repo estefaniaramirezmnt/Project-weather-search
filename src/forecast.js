347110c94496744dcdf9a2a4d37cf1022b9607f0
Estefanía <estefaniaramirezmnt@gmail.com>
2023-10-17 18:39:36 +0200

    Sent real data to forecast

--- src/forecast.js
+++ src/forecast.js
@@ -1,29 +1,34 @@
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecastData = response.data.daily;
+
   let forecastElement = document.getElementById("forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-        <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-            <img 
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-day.png"
-              alt="" 
-              width="42"
-            /> 
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">
-                18º
-              </span>
-              <span class="weather-forecast-temperature-min">
-                12º
-              </span>
-            </div>
+  forecastData.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML += `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatForecastDay(
+          forecastDay.time
+        )}</div>
+          <img
+            src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+              forecastDay.condition.icon
+            }.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+              ${Math.round(forecastDay.temperature.maximum)}º
+            </span>
+            <span class="weather-forecast-temperature-min">
+            ${Math.round(forecastDay.temperature.minimum)}º
+            </span>
+          </div>
       </div>
-      `;
+    `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
--- src/formatDate.js
+++ src/formatDate.js
@@ -1,23 +1,31 @@
 function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wensday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-  
-    return `${day} ${hours}:${minutes}`;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wensday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+
+  return days[day];
+}
