ae1e4317f981a1737157d061698d6e481daec76c
Estefanía <estefaniaramirezmnt@gmail.com>
2023-10-18 12:00:51 +0200

    ApiKey and commented lines not used (for now)

--- src/forecastConversion.js
+++ src/forecastConversion.js
@@ -1,39 +1,39 @@
-function forecastToFarenheit() {
-  let MaxtempNextDays = document.querySelectorAll(
-    ".weather-forecast-temperature-max"
-  );
-  let MintempNextDays = document.querySelectorAll(
-    ".weather-forecast-temperature-min"
-  );
+// function forecastToFarenheit() {
+//   let MaxtempNextDays = document.querySelectorAll(
+//     ".weather-forecast-temperature-max"
+//   );
+//   let MintempNextDays = document.querySelectorAll(
+//     ".weather-forecast-temperature-min"
+//   );
 
-  MaxtempNextDays.forEach(function (tempDay) {
-    let tempInCelsius = parseInt(tempDay.innerHTML);
-    let tempInFahrenheit = Math.round(tempInCelsius * 1.8 + 32);
-    tempDay.innerHTML = `${tempInFahrenheit}º`;
-  });
-  MintempNextDays.forEach(function (tempDay) {
-    let tempInCelsius = parseInt(tempDay.innerHTML);
-    let tempInFahrenheit = Math.round(tempInCelsius * 1.8 + 32);
-    tempDay.innerHTML = `${tempInFahrenheit}º`;
-  });
-}
+//   MaxtempNextDays.forEach(function (tempDay) {
+//     let tempInCelsius = parseInt(tempDay.innerHTML);
+//     let tempInFahrenheit = Math.round(tempInCelsius * 1.8 + 32);
+//     tempDay.innerHTML = `${tempInFahrenheit}º`;
+//   });
+//   MintempNextDays.forEach(function (tempDay) {
+//     let tempInCelsius = parseInt(tempDay.innerHTML);
+//     let tempInFahrenheit = Math.round(tempInCelsius * 1.8 + 32);
+//     tempDay.innerHTML = `${tempInFahrenheit}º`;
+//   });
+// }
 
-function forecastToCelsius() {
-  let MaxtempNextDays = document.querySelectorAll(
-    ".weather-forecast-temperature-max"
-  );
-  let MintempNextDays = document.querySelectorAll(
-    ".weather-forecast-temperature-min"
-  );
+// function forecastToCelsius() {
+//   let MaxtempNextDays = document.querySelectorAll(
+//     ".weather-forecast-temperature-max"
+//   );
+//   let MintempNextDays = document.querySelectorAll(
+//     ".weather-forecast-temperature-min"
+//   );
 
-  MaxtempNextDays.forEach(function (tempDay) {
-    tempDay.innerHTML = `${Math.round(celsiusMaxTemperature)}º`;
-  });
+//   MaxtempNextDays.forEach(function (tempDay) {
+//     tempDay.innerHTML = `${Math.round(celsiusMaxTemperature)}º`;
+//   });
 
-    MintempNextDays.forEach(function (tempDay) {
-        tempDay.innerHTML = `${Math.round(celsiusMinTemperature)}º`;
-    });
-}
+//     MintempNextDays.forEach(function (tempDay) {
+//         tempDay.innerHTML = `${Math.round(celsiusMinTemperature)}º`;
+//     });
+// }
 
-let celsiusMaxTemperature = null;
-let celsiusMinTemperature = null;
+// let celsiusMaxTemperature = null;
+// let celsiusMinTemperature = null;
